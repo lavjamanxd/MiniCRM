7e51ff74e7957506fdc65cf9d801705390757953
Balázs Koncz <balazs.koncz@casrd.net>
2019-04-24 11:17:50 +0200

    Add Appointment shared class

---
+++ MiniCRM.Shared/Appointment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniCRM.Shared
+{
+    public class Appointment
+    {
+        public string Keyword { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}

f40b25759568b0bc4ce97783a0b3e812252986be
Robert Szucs <robert.szucs@casrd.net>
2019-04-24 12:17:31 +0200

    Introduce IDataAccess

---
+++ MiniCRM.Server.DataAccess/IDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MiniCRM.Shared;
+
+namespace MiniCRM.Server.DataAccess
+
+{
+    public interface IDataAccess
+    {
+        Task<List<Appointment>> GetAppointments(int userId);
+        Task Save(Appointment appointment);
+    }
+}
